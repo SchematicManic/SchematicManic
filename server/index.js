954c7865e2671cb089665293a14a4f2e14872fa3
Jake Luton <jake.luton92@gmail.com>
2018-04-14 16:48:20 -0700

    Data chunking.

--- server/index.js
+++ server/index.js
@@ -8,13 +8,37 @@ const docSend = require('./docSend');
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+let body = '';
+  // Get the data as utf8 strings.
+  // If an encoding is not set, Buffer objects will be received.
+  req.setEncoding('utf8');
+
+  // Readable streams emit 'data' events once a listener is added
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  req.on('end', () => {
+    try {
+      const data = JSON.parse(body);
+      console.log('Chunked data: ', data);
+      // write back something interesting to the user:
+      // res.write(typeof data);
+      res.end();
+    } catch (er) {
+      // uh oh! bad json!
+      res.statusCode = 400;
+      return res.end(`error: ${er.message}`);
+    }
+  });
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.use('/docRequest', docRequest);
 
-app.use('/docSend', docSend);
+// app.use('/docSend', docSend);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
