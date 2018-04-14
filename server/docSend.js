07810b3aeee934ef4accdee07e5c9d11d5ab1f8b
Jake Luton <jake.luton92@gmail.com>
2018-04-14 16:49:42 -0700

    Chunking attempt 2.

--- server/docSend.js
+++ server/docSend.js
@@ -3,9 +3,30 @@ const router = express.Router();
 const lib = require('../lib');
 
 const docSend = function (req, res) {
-  console.log(req.method);
-  console.log('Req received from Design Forge: ', req.body);
-  res.sendStatus(201);
+  let body = '';
+  // Get the data as utf8 strings.
+  // If an encoding is not set, Buffer objects will be received.
+  req.setEncoding('utf8');
+
+  // Readable streams emit 'data' events once a listener is added
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  req.on('end', () => {
+    try {
+      const data = JSON.parse(body);
+      console.log('Chunked data: ', data);
+      // write back something interesting to the user:
+      // res.write(typeof data);
+      res.end();
+    } catch (er) {
+      // uh oh! bad json!
+      res.statusCode = 400;
+      return res.end(`error: ${er.message}`);
+    }
+  });
+
 };
 
 router.post('/', docSend);
