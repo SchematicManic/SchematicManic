5e1d6f030316978b48363cf1915f5d05ad9b1a59
Cody Schindler <admin@codyschindler.com>
2018-04-14 10:09:15 -0700

    server route for document created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+<<<<<<< HEAD
 const { AUTODESK_CLIENT_ID, AUTODESK_CLIENT_SECRET } = require('./config.js');
+=======
+const lib = require('../lib');
+>>>>>>> server route for document created
 
 const app = express();
 const PORT = 3000;
@@ -9,5 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.post('/docRequest', lib.docRequest);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
