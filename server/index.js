9beabffbbec908137ea96e42928cbce75f80892f
Cody Schindler <admin@codyschindler.com>
2018-04-14 12:04:09 -0700

    merge with upstream

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { AUTODESK_CLIENT_ID, AUTODESK_CLIENT_SECRET } = require('./config.js');
