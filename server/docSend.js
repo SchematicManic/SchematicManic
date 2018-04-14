0ffd805e901326aa89aa55d5b5aefdc1e9f7e82c
Jake Luton <jake.luton92@gmail.com>
2018-04-14 16:31:13 -0700

    Logs req body and method.

--- server/docSend.js
+++ server/docSend.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const lib = require('../lib');
 
 const docSend = function (req, res) {
-  console.log('Req received from Design Forge: ', req);
+  console.log(req.method);
+  console.log('Req received from Design Forge: ', req.body);
   res.sendStatus(201);
 };
 
 router.post('/', docSend);
+router.put('/', docSend);
 module.exports = router;
