972628e82f6ab37bd445867a96ae49c4fb3ea46e
Jake Luton <jake.luton92@gmail.com>
2018-04-14 17:38:52 -0700

    Downloads pdf

--- server/docRequest.js
+++ server/docRequest.js
@@ -3,19 +3,11 @@ const router = express.Router();
 const lib = require('../lib');
 
 const docRequest = async (req, res) => {
-<<<<<<< HEAD
   // let call = lib.generateAPICall(req.body);
 
 
   res.download('./result')
-=======
-  //generate API call (JSON object with appropriate auth and params)
-  let call = lib.generateAPICall(req.body);
-  //make API call, returns a file
-  // let file = await lib.callAutoForge(call);
-  //send back file as download
-  res.status(200).json(req.body); //delete me
->>>>>>> Created request handler for Design Forge Request.
+
   // res.status(200).download(file);
 };
 
--- server/docSend.js
+++ server/docSend.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 const lib = require('../lib');
+const docReady = require('./index.js');
 
 
 const docSend = function (req, res) {
